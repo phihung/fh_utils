783b585a0c6a0a14506246c22978c736c566a504
Phi Hung <phi@lajavaness.com>
2024-09-26 15:58:54 +0200

    chore: Release 0.4.0

--- src/fh_utils/__init__.py
+++ src/fh_utils/__init__.py
@@ -1,4 +1,4 @@
 from fh_utils.ipython_ext import load_ipython_extension  # noqa: F401
 from fh_utils.server import no_reload, no_reload_cache, serve  # noqa: F401
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
